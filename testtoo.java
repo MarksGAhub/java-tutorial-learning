11418865cd1a4910b3355955c1391b4efba5e994
MarksGAaccount <marksgaaccount@Marks-macbook.local>
2017-06-09 15:43:21 -0700

    Adding a second file

---
+++ testtoo.java
@@ -0,0 +1,6 @@
+public class HelloWorld {
+    public static void main(String []args){
+        // It's ok to take ones time to learn to do something correctly.
+        System.out.println("Hello World test two!");
+    }
+}
